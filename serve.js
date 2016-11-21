ef4ab25d82ef5036fd42422f1d3c6c3fe69c60d5
nicolas2lee <nicolas2lee@gmail.com>
2016-11-21 16:10:19 +0100

    updated prod

--- serve.js
+++ serve.js
@@ -1,7 +1,9 @@
 var config = require("./webpack.config.js");
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8090/");
+//config.entry.app.unshift("webpack-dev-server/client?http://localhost:8090/");
+config.entry.app.unshift("webpack-dev-server/client?https://limitless-falls-39025.herokuapp.com/");
+
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
     proxy: {
